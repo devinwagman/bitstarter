d9c2b54b56c591473af2f85269a6d4a712be7b1a
Devin Wagman <devin.wagman@gmail.com>
2013-07-14 05:46:51 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
 var buffer = new Buffer("Hello World from index.html", 25);
- fs.readFileSync("index.html", "utf-8");
+fs.readFileSync("index.html", "utf-8");
 response.send(resultOfreadFileSync);
-  });
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
